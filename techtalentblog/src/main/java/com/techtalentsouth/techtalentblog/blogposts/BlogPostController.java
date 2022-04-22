6935838091864949512b334df738a4173fa86682
rtoddm <101154587+rtoddm@users.noreply.github.com>
2022-04-22 21:41:51 +0300

    Add files via upload

---
+++ techtalentblog/src/main/java/com/techtalentsouth/techtalentblog/blogposts/BlogPostController.java
@@ -0,0 +1,46 @@
+package com.techtalentsouth.techtalentblog.blogposts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BlogPostController {
+	
+	//Tell SpringBoot to set this automatically via INJECTION
+	@Autowired
+	private BlogPostRepository blogPostRepository;
+	
+	private static List<BlogPost> posts = new ArrayList<>();
+	
+	
+	@GetMapping(value="/")
+	public String index(Model model) {
+		model.addAttribute("posts", posts);
+		return "blogpost/index";
+	}
+	
+	@GetMapping(value="/blogposts/new")
+	public String newBlog(BlogPost post) {
+		post.setAuthor("Todd");
+		return "blogpost/new";	
+	}
+	
+	@PostMapping(value="/blogposts")
+	public String addNewBlogPost(BlogPost blogPost, Model model) {
+		//This method's goal is to save a blogPost entered in the form to the database. 
+		BlogPost savedBlogPost = blogPostRepository.save(blogPost);
+		posts.add(savedBlogPost);
+		
+		model.addAttribute("title", savedBlogPost.getTitle());
+		model.addAttribute("author", savedBlogPost.getAuthor());
+		model.addAttribute("blogEntry", savedBlogPost.getBlogEntry());
+		
+		return "blogpost/result";
+	}
+}
